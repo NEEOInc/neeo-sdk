b536de3ea82c66bb4c509c7e2b801ea81a60d6c4
Niels de Klerk <Niels.dKlerk@vened.nl>
2017-05-08 14:23:51 +0200

    Added sanity checks, and removed async tests.

--- test/unit/lib/device/brain/urlbuilder_test.js
+++ test/unit/lib/device/brain/urlbuilder_test.js
@@ -6,28 +6,36 @@ const BASE_URL_GETRECIPES = '/v1/api/recipes';
 
 describe('./lib/device/brain/urlbuilder.js', function() {
 
-  it('should return brain url when object is provided', function(done) {
+  it('should return brain url when object is provided', function() {
     const test = urlbuilder.buildBrainUrl({host:'test.local', port:6336});
     expect(test).to.equal('http://test.local:6336');
-    done();
   });
 
-  it('should return brain url when string is provided', function(done) {
+  it('should return brain url when string is provided', function() {
     const test = urlbuilder.buildBrainUrl('brain.local');
     expect(test).to.equal('http://brain.local:3000');
-    done();
   });
   
-  it('should return brain url and path when object is provided with uri', function(done) {
+  it('should return brain url and path when object is provided with uri', function() {
     const test = urlbuilder.buildBrainUrl({host:'test.local', port:6336}, BASE_URL_GETRECIPES);
     expect(test).to.equal('http://test.local:6336/v1/api/recipes');
-    done();
   });
 
-  it('should return brain url and path when string is provided with uri', function(done) {
+  it('should return brain url and path when string is provided with uri', function() {
     const test = urlbuilder.buildBrainUrl('brain.local', BASE_URL_GETRECIPES);
     expect(test).to.equal('http://brain.local:3000/v1/api/recipes');
-    done();
+  });
+
+  it('should fail to create url, with missing parameter', function() {
+    expect(function() {
+      urlbuilder.buildBrainUrl();
+    }).to.throw(/URLBUILDER_INVALID_PARAMETER_BRAIN/);
+  });
+
+    it('should fail to create url, with invalid parameter', function() {
+    expect(function() {
+      urlbuilder.buildBrainUrl(6336);
+    }).to.throw(/URLBUILDER_INVALID_PARAMETER_BRAIN/);
   });
 
 });
